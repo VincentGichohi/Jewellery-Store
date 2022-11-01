c2ef502d9f86bf51380e1ca6eed01426266fd355
VincentGichohi <vincentegichohi@gmail.com>
2022-11-01 12:36:47 +0300

    add a return statement to address model

--- store/models.py
+++ store/models.py
@@ -4,4 +4,9 @@ from django.contrib.auth.models import User
 
 class Address(models.Model):
     user = models.ForeignKey(User, verbose_name="User", on_delete=models.CASCADE)
+    locality = models.CharField(max_length=150, verbose_name='Nearest Location')
+    city = models.CharField(max_length=150, verbose_name='State')
+    
+    def __str__(self) -> str:
+        return self.locality
     
