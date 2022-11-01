4c2dae68aa7c359d31b1986af928fe50de89c62f
VincentGichohi <vincentegichohi@gmail.com>
2022-11-01 17:56:43 +0300

    add setpasswordform

--- store/forms.py
+++ store/forms.py
@@ -48,3 +48,9 @@ class PasswordChangeForm(PasswordChangeForm):
 
 class PasswordResetForm(PasswordResetForm):
     email = forms.EmailField(label=_("Email"), max_length=254, widget=forms.EmailInput(attrs={'autocomplete':'email', 'class':'form-control'}))
+
+
+class SetPasswordForm(SetPasswordForm):
+    new_password1 = forms.CharField(label=_("New Password"), strip=False, widget=forms.PasswordInput(attrs={'autocomplete':'new-password', 'class':'form-control'}), help_text=password_validation.password_validators_help_text_html())
+    new_password2 = forms.CharField(label=_("Confirm Password"), strip=False, widget=forms.PasswordInput(attrs={'autocomplete':'new-password','class':'form-control'}))
+
