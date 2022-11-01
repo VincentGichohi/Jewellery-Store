3a546afe84a8b441f7f6f88714eb4d59d67d4f69
VincentGichohi <vincentegichohi@gmail.com>
2022-11-01 13:02:06 +0300

    add a product model

--- store/models.py
+++ store/models.py
@@ -28,4 +28,27 @@ class Category(models.Model):
     def __str__(self) -> str:
         return self.title
     
+
+class Product(models.Model):
+    title = models.CharField(max_length=150, verbose_name="Product Title")
+    slug = models.SlugField(max_length=160, verbose_name="Product Slug")
+    sku = models.CharField(max_length=255, unique=True, verbose_name="Unique Product ID(SKU)")
+    short_description = models.TextField(verbose_name="Short Description")
+    detail_description = models.TextField(blank=True, null=True, verbose_name="Detail Description")
+    product_image = models.ImageField(upload_to="product", blank=True, null=True, verbose_name="Product Image")
+    price = models.DecimalField(max_digits=8, decimal_places=2)
+    category = models.ForeignKey(Category, verbose_name="Product Category", on_delete=models.CASCADE)
+    is_active = models.BooleanField(verbose_name="Is Active?")
+    is_featured = models.BooleanField(verbose_name="Is Featured?")
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name="Created Date")
+    updated_at = models.DateTimeField(auto_add=True, verbose_name="Updated Date")
+
+    class Meta:
+        verbose_name_plural = "Products"
+        ordering =  ('-created_at', )
+
+    def __str__(self):
+        return self.title
+
+    
     
