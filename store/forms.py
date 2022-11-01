a9d37a34ab705fdb7d5132ad9b77a2edc8477b44
VincentGichohi <vincentegichohi@gmail.com>
2022-11-01 17:50:43 +0300

    add addressform

--- store/forms.py
+++ store/forms.py
@@ -26,3 +26,15 @@ class RegistrationForm(UserCreationForm):
 class LoginForm(AuthenticationForm):
     username = UsernameField(widget=forms.TextInput(attrs={'autofocus': True, 'class': 'form-control'}))
     password = forms.CharField(label=_("Password"), strip=False, widget=forms.PasswordInput(attrs={'autocomplete': 'current-password', 'class': 'form-control'}))
+
+
+class AddressForm(forms.ModelForm):
+    class Meta:
+        model = Address
+        fields = ['locality', 'city', 'state']
+        widgets = {'locality': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Popular Place like '
+                                                                                              'Restaurant, '
+                                                                                              'Religious Site, '
+                                                                                              'etc.'}),
+                   'city': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'City'}),
+                   'state': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'State or Province'})}
