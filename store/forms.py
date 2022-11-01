a1ad1a983f0b0cc1ab43dcc2ca0156ddb288e672
VincentGichohi <vincentegichohi@gmail.com>
2022-11-01 17:42:20 +0300

    add registrationform

--- store/forms.py
+++ store/forms.py
@@ -9,3 +9,15 @@ from django.db.models import fields
 from django.forms import widgets
 from django.forms.fields import CharField
 from django.utils.translation import gettext, gettext_lazy as _
+
+
+class RegistrationForm(UserCreationForm):
+    password1 = forms.CharField(label='Password', widget=forms.PasswordInput(attrs={'class': 'form-control', 'placeholder': 'Password'}))
+    password2 = forms.CharField(label="Confirm Password", widget=forms.PasswordInput(attrs={'class': 'form-control', 'placeholder': 'Confirm Password'}))
+    email = forms.CharField(required=True, widget=forms.EmailInput(attrs={'class': 'form-control', 'placeholder': 'Email Address'}))
+
+    class Meta:
+        model = User
+        fields = ['username', 'email', 'password1', 'password2']
+        label = {'email': 'Email'}
+        widgets = {'username': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Username'})}
