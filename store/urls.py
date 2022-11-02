1202fc3e433270d9255ca197f51138e43231c757
VincentGichohi <vincentegichohi@gmail.com>
2022-11-02 12:27:27 +0300

    add paths for cart and checkout]

--- store/urls.py
+++ store/urls.py
@@ -5,3 +5,15 @@ from django.contrib.auth import views as auth_views
 
 app_name = 'store'
 
+urlpatterns = [
+    path('', views.home, name="home"),
+    # URL for Cart and Checkout
+    path('add-to-cart/', views.add_to_cart, name="add-to-cart"),
+    path('remove-cart/<int:cart_id>/', views.remove_cart, name="remove-cart"),
+    path('plus-cart/<int:cart_id>/', views.plus_cart, name="plus-cart"),
+    path('minus-cart/<int:cart_id>/', views.minus_cart, name="minus-cart"),
+    path('cart/', views.cart, name="cart"),
+    path('checkout/', views.checkout, name="checkout"),
+    path('orders/', views.orders, name="orders"),
+
+]
