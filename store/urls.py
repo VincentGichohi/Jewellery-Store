7ae2b0937c3b137c1b406e87c0fb1e7d3b67f34c
VincentGichohi <vincentegichohi@gmail.com>
2022-11-02 12:26:37 +0300

    add app name to app url file

--- store/urls.py
+++ store/urls.py
@@ -2,3 +2,6 @@ from django.urls import path
 from store.forms import LoginForm, PasswordChangeForm, PasswordResetForm, SetPasswordForm
 from . import views
 from django.contrib.auth import views as auth_views
+
+app_name = 'store'
+
