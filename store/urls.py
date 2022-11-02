97a6eeeabf013707627f98e9e09e37cb860dc904
VincentGichohi <vincentegichohi@gmail.com>
2022-11-02 12:28:36 +0300

    add paths for products

--- store/urls.py
+++ store/urls.py
@@ -16,4 +16,10 @@ urlpatterns = [
     path('checkout/', views.checkout, name="checkout"),
     path('orders/', views.orders, name="orders"),
 
+    # URL for Products
+    path('product/<slug:slug>/', views.detail, name="product-detail"),
+    path('categories/', views.all_categories, name="all-categories"),
+    path('<slug:slug>/', views.category_products, name="category-products"),
+
+    path('shop/', views.shop, name="shop"),
 ]
