1c9f6e1f0087f9d42a66be35df0c077de35a0ad1
VincentGichohi <vincentegichohi@gmail.com>
2022-11-02 11:34:52 +0300

    add imports to vies

--- store/views.py
+++ store/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from django.contrib.auth.models import User
+from .models import Address, Cart, Category, Order, Product
+from .forms import RegistrationForm, AddressForm
+from django.views import View
+import decimal
+from django.contrib.auth.decorators import login_required
+from django.utils.decorators import method_decorator # For class based views
 
-# Create your views here.
