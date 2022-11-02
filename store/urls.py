0e19ab9d69ae9fb9aadc0b6a7f9903e16500af00
VincentGichohi <vincentegichohi@gmail.com>
2022-11-02 12:25:19 +0300

    add imports to app urls file

--- store/urls.py
+++ store/urls.py
@@ -1,1 +1,4 @@
 from django.urls import path
+from store.forms import LoginForm, PasswordChangeForm, PasswordResetForm, SetPasswordForm
+from . import views
+from django.contrib.auth import views as auth_views
