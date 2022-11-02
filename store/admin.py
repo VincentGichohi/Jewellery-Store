c6e4f510bc0aa33368bb0340eeee6a7eebdcb2eb
VincentGichohi <vincentegichohi@gmail.com>
2022-11-02 08:53:24 +0300

    add addressadmin class

--- store/admin.py
+++ store/admin.py
@@ -1,2 +1,9 @@
 from django.contrib import admin
 from .models import Address, Category, Product, Cart, Order
+
+
+class AddressAdmin(admin.ModelAdmin):
+    list_display = ('user', 'locality', 'city', 'state')
+    list_filter = ('city', 'state')
+    list_per_page = 10
+    search_fields = ('locality', 'city', 'state')
